f09132ddd128cd26a1e9f591b05dc581d28c3efa
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-09-20 17:47:48 -0300

    1

---
+++ app/src/main/java/com/example/myapplication/Bem_VindoActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bem_VindoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bem_vindo_layout);
+    }
+}
